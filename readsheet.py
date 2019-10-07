c806c1a184b737a2fb43daf731945e0e77c82c6e
Nicholas Hobbs <nick.hobbs42@gmail.com>
2019-10-07 14:34:52 +1100

    Modified to generate authorized_keys file

--- readsheet.py
+++ readsheet.py
@@ -1,11 +1,17 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('homenetwork_secret.json', scope)
-client = gspread.authorize(creds)
+def getKeys():
+	scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+	creds = ServiceAccountCredentials.from_json_keyfile_name('homenetwork_secret.json', scope)
+	client = gspread.authorize(creds)
 
-sheet = client.open('Home Network').worksheet('SSH Keys')
+	sheet = client.open('Home Network').worksheet('SSH Keys')
 
-network = sheet.get_all_records()
-print(network)
+	keys = sheet.get_all_records()
+	return keys
+
+if __name__ == "__main__":
+	allKeys = getKeys()
+	for key in allKeys:
+		print(key['Key'])
