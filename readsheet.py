c7e925582f10acd82557488b33f67f2023f7a2a9
Nicholas Hobbs <nick.hobbs42@gmail.com>
2019-10-20 17:07:30 +1100

    Implemented generating .ssh/config file

--- readsheet.py
+++ readsheet.py
@@ -4,27 +4,37 @@ import sys
 from oauth2client.service_account import ServiceAccountCredentials
 
 # connect to the google sheet and get the contents of the SSH Keys sheet
-def getKeys():
+def getSheet(sheetName):
 	scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 	creds = ServiceAccountCredentials.from_json_keyfile_name('homenetwork_secret.json', scope)
 	client = gspread.authorize(creds)
 
-	sheet = client.open('Home Network').worksheet('SSH Keys')
+	sheet = client.open('Home Network').worksheet(sheetName)
 
 	keys = sheet.get_all_records()
 	return keys
 
 # generate authorized_keys file from column name
 def genAuthKeys(colName=""):
-	allKeys = getKeys()
+	allKeys = getSheet("SSH Keys")
 	for key in allKeys:
 		if colName == "":
 			print(key['Key'])
 		elif key[colName] == 'TRUE':
 			print(key['Key'])
 
+# generate .ssh/config file
+def getSSHConfig():
+	schema = getSheet("IP Schema")
+	for device in schema:
+		if (device['SSH Alias'] != ""):
+			print("Host " + device['SSH Alias'])
+			print(" "*4 + "HostName " + device['IP Address'])
+			if (device['SSH Port'] != ""):
+				print(" "*4 + "Port " + str(device['SSH Port']))
+			if (device['SSH User'] != ""):
+				print(" "*4 + "User " + device['SSH User'])
+
+
 if __name__ == "__main__":
-	if len(sys.argv) == 1:
-		genAuthKeys()
-	else:
-		genAuthKeys(sys.argv[1])
+	getSSHConfig()
