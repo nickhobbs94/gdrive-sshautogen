c83279c68d045a39eff403d55afeb7eb5d431335
Nicholas Hobbs <nick.hobbs42@gmail.com>
2019-10-20 16:44:55 +1100

    Converted __name__==__main__ section to a function

--- readsheet.py
+++ readsheet.py
@@ -3,6 +3,7 @@ import gspread
 import sys
 from oauth2client.service_account import ServiceAccountCredentials
 
+# connect to the google sheet and get the contents of the SSH Keys sheet
 def getKeys():
 	scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 	creds = ServiceAccountCredentials.from_json_keyfile_name('homenetwork_secret.json', scope)
@@ -13,10 +14,17 @@ def getKeys():
 	keys = sheet.get_all_records()
 	return keys
 
-if __name__ == "__main__":
+# generate authorized_keys file from column name
+def genAuthKeys(colName=""):
 	allKeys = getKeys()
 	for key in allKeys:
-		if len(sys.argv) == 1:
+		if colName == "":
 			print(key['Key'])
-		elif key[sys.argv[1]] == 'TRUE':
+		elif key[colName] == 'TRUE':
 			print(key['Key'])
+
+if __name__ == "__main__":
+	if len(sys.argv) == 1:
+		genAuthKeys()
+	else:
+		genAuthKeys(sys.argv[1])
